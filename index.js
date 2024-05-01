be6419ac6a667d0d83963e939026c9426669a711
Sarita <sm7767119@gmail.com>
2024-05-01 23:18:13 +0530

    mongodb with expressjs

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 main()
   .then(() => {
@@ -36,7 +38,34 @@ chat1
 app.get("/chats", async (req, res) => {
   let chats = await Chat.find();
   console.log(chats);
-  res.send("working");
+  res.render("index.ejs", { chats });
+});
+
+// new Route
+
+app.get("/chats/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+// Create Route
+
+app.post("/chats", (req, res) => {
+  let { from, to, msg } = req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    msg: msg,
+    created_at: new Date(),
+  });
+  newChat
+    .save()
+    .then((res) => {
+      console.log("chat was saved");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/chats");
 });
 
 app.get("/", (req, res) => {
